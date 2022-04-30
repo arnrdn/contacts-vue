e84290d7a5a231429c8f9e7a8f60165d9afa4291
rdnrn <rodina.arina00@gmail.com>
2022-04-30 12:19:10 +0300

    Setup the project

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,61 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
+  state: () => ({
+    contacts: [],
+    isModalOpen: false,
+    editId: 0,
+  }),
+  getters: {},
   mutations: {
+    setContacts(state, contacts) {
+      state.contacts = contacts;
+    },
+    setModalOpen(state, bool) {
+      state.isModalOpen = bool;
+    },
+    setEditId(state, id) {
+      state.editId = id;
+    },
+    removeContactFromList(state, contact) {
+      state.contacts = state.contacts.filter((c) => c.id !== contact.id);
+    },
+    addContactToList(state, contact) {
+      state.contacts.push(contact);
+      console.log(state.contacts);
+    },
+    editContactById(state, newContact) {
+      const id = state.editId;
+      const itemIndex = state.contacts.findIndex((c) => c.id === id);
+      state.contacts[itemIndex] = newContact;
+    },
   },
   actions: {
+    async fetchContacts({ commit }) {
+      try {
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        commit('setContacts', response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    removeContact({ commit }, contact) {
+      commit('removeContactFromList', contact);
+    },
+    addContact({ commit }, contact) {
+      commit('addContactToList', contact);
+    },
+    editContact({ commit }, newContact) {
+      commit('editContactById', newContact);
+    },
+    commitIsModalOpen({ commit }, bool) {
+      commit('setModalOpen', bool);
+    },
+    commitSetEditId({ commit }, id) {
+      commit('setEditId', id);
+    },
   },
-  modules: {
-  }
-})
+});
---
+++ src/views/ContactsPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <modal-edit-contact></modal-edit-contact>
+    <router-link to="/">Home</router-link>
+    <h1>This is an about page</h1>
+    <add-contact></add-contact>
+    <contacts-list></contacts-list>
+  </div>
+</template>
+
+<script>
+import ModalEditContact from '@/components/ModalEditContact.vue';
+import AddContact from '@/components/AddContact.vue';
+import ContactsList from '@/components/ContactsList.vue';
+export default {
+  components: {
+    ContactsList,
+    AddContact,
+    ModalEditContact,
+  },
+};
+</script>
